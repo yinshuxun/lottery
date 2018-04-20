fc977b6c84cd378a7df1705f0605e8cef808b9bf
yinshuxun <sxyin@alauda.io>
2018-04-20 17:24:43 +0800

    add some basic route

--- server/index.ts
+++ server/index.ts
@@ -3,58 +3,54 @@ import * as Router from 'koa-router'
 import * as Logger from 'koa-logger'
 import * as cheerio from 'cheerio'
 import axios from 'axios'
-import { objectSort, objKeySort } from './utils';
+import { objectSort, objKeySort, getUrl, getData, statisticsWithMoney, statistics, genToSL } from './utils';
+import cache from './data'
 
 const app = new Koa()
 const router = new Router()
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const testData = [[0], [0, 0], [0, 0, 0], [0, 0, 0, 0], [23], [23, 23], [23, 23, 23], [23, 23, 23, 23], [0, 23, 0, 23], [0, 23, 23, 0], [0, 23, 23, 23, 0], [0, 23, 23, 23, 23, 0], [0, 23, 23, 23, 23, 23, 0, 23]]
+const correctResult = [9.5, 19, 28.5, 38, -10, -30, -70, -32, 8.5, 17.5, -100.5, -62.5, -34.5]
 
-const RESULTS_URL = 'https://hgw666.tv/app/member/Lottery/list.php?t='
-
-const getUrl = (t: number = 1) => {
-    return `${RESULTS_URL}${t}`
-}
-const genToSL = (arr: number[]): string[] => {
-    return arr.map(i => {
-        return i <= 22 ? '小' : '大'
-    })
-}
-
-const statistics = (arr: number[]) => {
-    const initArr = genToSL(arr)
-    initArr.push('占位')
-    let pre_i = 0, pre_v = initArr[0], next, result = {}
-    initArr.forEach((v, i) => {
-        if (v !== pre_v) {
-            if (result[`${i - pre_i}连${pre_v}`]) {
-                result[`${i - pre_i}连${pre_v}`]++
-            } else {
-                result[`${i - pre_i}连${pre_v}`] = 1
-            }
-            pre_i = i
-            pre_v = v
+router.get('/cache/:id/:end', async ctx => {
+    const ret = await axios.get(getUrl(ctx.params.id))
+    const start = ctx.params.id
+    const end = ctx.params.end
+    let i = start, result = {};
+    while (i <= end) {
+        if (cache[i]) {
+            result[i] = cache[i]
+        } else {
+            result[i] = await getData(i)
         }
-    })
-    return result
-}
-
-const getData = async (t: number = 1) => {
-    const ret = await axios.get(getUrl(t))
-    const $ = cheerio.load(ret.data)
-    const newArr = []
-    $('.line_list').each((index: number, ele: CheerioElement) => {
-        const text = $($(ele).children()[3]).html()
-        if (text) {
-            newArr.push(text.slice(0, 2))
-        }
-    })
-    newArr.reverse()
-    return statistics(newArr)
-}
+        console.log(`请求第${i}条数据`)
+        i++
+    }
+    ctx.body = result
+})
 
 router.get('/page/:id', async ctx => {
     const ret = await axios.get(getUrl(ctx.params.id))
     ctx.body = ret.data
+})
+
+router.get('/lalala', async ctx => {
+    const start = 1
+    let end = 100
+    let i = start, result = [];
+    while (end >= i) {
+        const curr = await getData(end--)
+        result = result.concat(genToSL(curr))
+    }
+    const resultString = result.join('')
+    const space = resultString.slice(resultString.length - 5);
+    const littleTimes = resultString.match(new RegExp(space + '小', 'ig')).length
+    const largeTimes = resultString.match(new RegExp(space + '大', 'ig')).length
+    ctx.body = `
+    <div style="flex:1;margin-bottom:20px;font-size:30px;color:grey;display:flex;text-align:center;">
+    预测时间:${new Date().getHours()}：${new Date().getMinutes()}</div>
+    <div style="flex:1;font-size:30px;display:flex;text-align:center;">大：出现${largeTimes}次，占比 ${Math.round(largeTimes / (largeTimes + littleTimes) * 100)}%;
+    <br>小：出现${littleTimes}次，占比 ${Math.round(littleTimes / (largeTimes + littleTimes) * 100)}%<div>`
 })
 
 router.get('/total/:id/:end', async ctx => {
@@ -76,22 +72,65 @@ router.get('/total/:id/:end', async ctx => {
     }
 })
 
+router.get('/string/:id/:end', async ctx => {
+    const start = ctx.params.id
+    let end = ctx.params.end
+    let i = start, result = [];
+    while (end >= i) {
+        const curr = await getData(end--)
+        result = result.concat(genToSL(curr))
+    }
+    ctx.body = result.join('')
+})
+
+router.get('/test', async (ctx) => {
+    ctx.body = statisticsWithMoney([23, 23, 23, 0])
+})
+
+router.get('/money/:id/:end', async (ctx) => {
+    const start = ctx.params.id
+    let end = ctx.params.end
+    let i = start, result = {}, totalD = 0, totalX = 0;
+    while (end >= i) {
+        const currD = statisticsWithMoney(await getData(end), '大')
+        const currX = statisticsWithMoney(await getData(end), '小')
+        totalD += currD
+        totalX += currX
+        result[`第${end}天的数据`] = `买大：${currD}, 买小：${currX}`
+        end--
+    }
+    result['合计'] = `买大：${totalD}, 买小：${totalX}`
+    ctx.body = result
+})
+
+router.get('/init/:id', async (ctx) => {
+    ctx.body = await getData(ctx.params.id)
+})
+
+router.get('/money/:id', async (ctx) => {
+    ctx.body = `盈亏：${statisticsWithMoney(await getData(ctx.params.id))}`
+})
+
+router.get('/moneytest', async (ctx) => {
+    ctx.body = {
+        from: testData.map(v => statisticsWithMoney(v)),
+        exepct: correctResult
+    }
+})
+
 router.get('/:id/:end', async ctx => {
     const start = ctx.params.id
     const end = ctx.params.end
     let i = start, result = {};
     while (i <= end) {
-        result[`第${i}天的数据`] = objKeySort(await getData(i++))
+        result[`第${i}天的数据`] = objKeySort(statistics(await getData(i++)))
     }
     ctx.body = result
 })
 
-
-
 router.get('/:id', async ctx => {
-    ctx.body = objKeySort(await getData(ctx.params.id))
+    ctx.body = objKeySort(statistics(await getData(ctx.params.id)))
 })
-
 
 app.use(Logger())
     .use(router.routes())
--- server/utils.ts
+++ server/utils.ts
@@ -1,3 +1,9 @@
+import axios from 'axios'
+import * as cheerio from 'cheerio'
+import cache from './data'
+
+const RESULTS_URL = 'https://hgw666.tv/app/member/Lottery/list.php?t='
+
 export const transformAudienceNumber = text => {
     return text.indexOf('万') > 0 ? text.replace(/万/, '') * 10000 : text;
 }
@@ -16,5 +22,253 @@ export const objKeySort = (arys) => {
     for (var i = 0; i < newkey.length; i++) {
         newObj[newkey[i]] = arys[newkey[i]];
     }
-    return newObj; 
+    return newObj;
+}
+
+export const getUrl = (t: number = 1) => {
+    return `${RESULTS_URL}${t}`
+}
+
+export const genToSL = (arr: number[]): string[] => {
+    return arr.map ? arr.map(i => {
+        return i <= 22 ? '小' : '大'
+    }) : []
+    // return arr.map ? arr.map(i => {
+    //     return i % 2 === 0 ? '双' : '单'
+    // }) : []
+}
+
+export const statistics = (arr: number[]) => {
+    const initArr = genToSL(arr)
+    initArr.push('占位')
+    let pre_i = 0, pre_v = initArr[0], next, result = {}
+    initArr.forEach((v, i) => {
+        if (v !== pre_v) {
+            if (result[`${i - pre_i}连${pre_v}`]) {
+                result[`${i - pre_i}连${pre_v}`]++
+            } else {
+                result[`${i - pre_i}连${pre_v}`] = 1
+            }
+            pre_i = i
+            pre_v = v
+        }
+    })
+    return result
+}
+
+export const getData = async (t: number = 1) => {
+    if (cache[t] && cache[t].length) {
+        return cache[t]
+    }
+    const ret = await axios.get(getUrl(t))
+    const $ = cheerio.load(ret.data)
+    const newArr = []
+    $('.line_list').each((index: number, ele: CheerioElement) => {
+        const text = $($(ele).children()[3]).html()
+        if (text) {
+            newArr.push(text.slice(0, 2))
+        }
+    })
+    newArr.reverse()
+    return newArr
+}
+
+//连三升级版
+export const statisticsWithMoney = (arr: number[], baseChoose: string = '大', basePayoff: number = 10, maxSeries: number = 3, percent = 1.95) => {
+    const initArr = genToSL(arr)
+    let currMoney = 0, pre_v = initArr[0], pre_i = 0;
+    initArr.forEach((v, i) => {
+        if (v === baseChoose && pre_v === v) {
+            if (i - pre_i >= 3) {
+                currMoney -= basePayoff
+                currMoney += basePayoff * 1.95
+            } else {
+                currMoney -= basePayoff
+                currMoney += basePayoff * 1.95
+            }
+        } else if (v === baseChoose && pre_v !== v) {
+            if (i - pre_i === 1) {
+                currMoney -= basePayoff * 1
+                currMoney += basePayoff * 1 * 1.95
+            } else if (i - pre_i === 2) {
+                currMoney -= basePayoff * 2
+                currMoney += basePayoff * 2 * 1.95
+            } else if (i - pre_i >= 3) {
+                currMoney -= basePayoff * 3.5
+            }
+            pre_v = v
+            pre_i = i
+        } else if (v !== baseChoose && pre_v === v) {
+            if (i - pre_i === 1) {
+                currMoney -= basePayoff * 1
+            } else if (i - pre_i === 2) {
+                currMoney -= basePayoff * 2
+            } else if (i - pre_i >= 3) {
+                currMoney -= basePayoff * 3.5
+                currMoney += basePayoff * 3.5 * 1.95
+            }
+        } else if (v !== baseChoose && pre_v !== v) {
+            if (i - pre_i >= 3) {
+                currMoney -= basePayoff
+            } else {
+                currMoney -= basePayoff
+            }
+            pre_v = v
+            pre_i = i
+        }
+    })
+    return currMoney
+}
+
+//无脑跟 todo
+export const statisticsWithMoney01 = (arr: number[], basePayoff: number = 10, baseChoose: string = '小', maxSeries: number = 3, percent = 1.95) => {
+    const initArr = genToSL(arr)
+    let currMoney = 0, pre_v = initArr[0], pre_i = 0, transport = 0;
+    initArr.forEach((v, i) => {
+        if (v === baseChoose && pre_v === v) {
+            transport = (2 * transport + basePayoff) * 1.1
+            currMoney -= basePayoff
+            currMoney += basePayoff * 1.95
+            transport = 0
+        } else if (v === baseChoose && pre_v !== v) {
+            transport = (2 * transport + basePayoff) * 1.1
+            currMoney -= transport
+            currMoney += transport * 1.95
+            transport = 0
+            pre_v = v
+            pre_i = i
+        } else if (v !== baseChoose && pre_v === v) {
+            const newTransport = (transport + basePayoff) * 1.1
+            currMoney -= newTransport
+            transport += newTransport
+        } else if (v !== baseChoose && pre_v !== v) {
+            const newTransport = (transport + basePayoff) * 1.1
+            currMoney -= transport
+            transport += newTransport
+            pre_v = v
+            pre_i = i
+        }
+    })
+    return currMoney
+}
+
+//连三升级版
+export const statisticsWithMoney22 = (arr: number[], basePayoff: number = 10, baseChoose: string = '双', maxSeries: number = 3, percent = 1.95) => {
+    const initArr = genToSL(arr)
+    let currMoney = 0, pre_v = initArr[0], pre_i = 0;
+    initArr.forEach((v, i) => {
+        if (v === baseChoose && pre_v === v) {
+            if (i - pre_i >= 3) {
+                currMoney -= basePayoff * 4
+                currMoney += basePayoff * 4 * 1.95
+            } else {
+                currMoney -= basePayoff
+                currMoney += basePayoff * 1.95
+            }
+        } else if (v === baseChoose && pre_v !== v) {
+            if (i - pre_i === 1) {
+                currMoney -= basePayoff * 1
+                currMoney += basePayoff * 1 * 1.95
+            } else if (i - pre_i === 2) {
+                currMoney -= basePayoff * 2
+                currMoney += basePayoff * 2 * 1.95
+            } else if (i - pre_i >= 3) {
+                currMoney -= basePayoff * 4
+            }
+            pre_v = v
+            pre_i = i
+        } else if (v !== baseChoose && pre_v === v) {
+            if (i - pre_i === 1) {
+                currMoney -= basePayoff * 1
+            } else if (i - pre_i === 2) {
+                currMoney -= basePayoff * 2
+            } else if (i - pre_i >= 3) {
+                currMoney -= basePayoff * 4
+                currMoney += basePayoff * 4 * 1.95
+            }
+        } else if (v !== baseChoose && pre_v !== v) {
+            if (i - pre_i >= 3) {
+                currMoney -= basePayoff * 4
+            } else {
+                currMoney -= basePayoff
+            }
+            pre_v = v
+            pre_i = i
+        }
+    })
+    return currMoney
+}
+
+//连输三把 跟进连续的
+export const statisticsWithMoney_1 = (arr: number[], basePayoff: number = 10, baseChoose: string = '大', maxSeries: number = 3, percent = 1.95) => {
+    const initArr = genToSL(arr)
+    let currMoney = 0, pre_v = initArr[0], pre_i = 0;
+    initArr.forEach((v, i) => {
+        if (v === baseChoose && pre_v === v) {
+            currMoney -= basePayoff
+            currMoney += basePayoff * 1.95
+        } else if (v === baseChoose && pre_v !== v) {
+            if (i - pre_i === 1) {
+                currMoney -= basePayoff * 2
+                currMoney += basePayoff * 2 * 1.95
+            } else if (i - pre_i === 2) {
+                currMoney -= basePayoff * 4
+                currMoney += basePayoff * 4 * 1.95
+            } else if (i - pre_i >= 3) {
+                currMoney -= basePayoff * 4
+            }
+            pre_v = v
+            pre_i = i
+        } else if (v !== baseChoose && pre_v === v) {
+            if (i - pre_i === 0) {
+                currMoney -= basePayoff * 1
+            } else if (i - pre_i === 1) {
+                currMoney -= basePayoff * 2
+            } else if (i - pre_i === 2) {
+                currMoney -= basePayoff * 4
+            } else if (i - pre_i >= 3) {
+                currMoney -= basePayoff * 4
+                currMoney += basePayoff * 4 * 1.95
+            }
+        } else if (v !== baseChoose && pre_v !== v) {
+            currMoney -= basePayoff
+            pre_v = v
+            pre_i = i
+        }
+    })
+    return currMoney
+}
+
+
+//错误翻倍法 ，肯定不行
+export const statisticsWithMoney11 = (arr: number[], basePayoff: number = 10, baseChoose: string = '大', maxSeries: number = 3, percent = 1.95) => {
+    const initArr = genToSL(arr)
+    let currMoney = 0, pre_v = initArr[0], pre_i = 0;
+    initArr.forEach((v, i) => {
+        if (v === baseChoose && pre_v === v) {
+            currMoney -= basePayoff
+            currMoney += basePayoff * 1.95
+        } else if (v === baseChoose && pre_v !== v) {
+            if (i - pre_i === 1) {
+                currMoney -= basePayoff * 2
+                currMoney += basePayoff * 2 * 1.95
+            } else if (i - pre_i >= 2) {
+                currMoney -= basePayoff * 2 * (i - pre_i)
+                currMoney += basePayoff * 2 * (i - pre_i) * 1.95
+            }
+            pre_v = v
+            pre_i = i
+        } else if (v !== baseChoose && pre_v === v) {
+            if (i - pre_i === 0) {
+                currMoney -= basePayoff * 1
+            } else if (i - pre_i >= 1) {
+                currMoney -= basePayoff * 2 * (i - pre_i)
+            }
+        } else if (v !== baseChoose && pre_v !== v) {
+            currMoney -= basePayoff
+            pre_v = v
+            pre_i = i
+        }
+    })
+    return currMoney
 }
